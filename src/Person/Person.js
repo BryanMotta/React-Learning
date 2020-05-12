4e0c8b8d137878936b4e76cff864edb1a8ae0bb0
Bryan Correia <bryanmotta7@gmail.com>
2020-05-12 15:44:15 -0300

    Component ErrorBoundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,12 +2,6 @@ import React from 'react';
 import personCss from "./Person.module.css"
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    }
-
     return (
         <div className={personCss.Person}>
             <p onClick={props.click}> I'm a {props.name} and i am {props.age} years old ! </p>
